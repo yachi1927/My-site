c98b6d184e2b9e9bd70aad2f3542d11e6e77a8de
yachi1927 <142282801+yachi1927@users.noreply.github.com>
2024-10-28 20:54:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-'use strict';
+"use strict";
 
+let currentIndex = 0;
+
+function moveSlide(direction) {
+  const slides = document.querySelectorAll(".carousel-slides img");
+  const totalSlides = slides.length;
+
+  currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+  const offset = -currentIndex * 100;
+
+  document.querySelector(
+    ".carousel-slides"
+  ).style.transform = `translateX(${offset}%)`;
+}
